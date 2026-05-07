63f4da78ab7c3847e09324aee98017de13b4ca3d
Anurag Sharma <yourboyanurag777@gmail.com>
2026-05-07 10:21:08 +0530

    Update connect.ts

--- db/connect.ts
+++ db/connect.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connect = async () => {
     try {
         mongoose.connect(process.env.MONGODB_URI || "")
+        
     } catch (err: any) {
 
         console.log(`The error is ${err.message}`)
